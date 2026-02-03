aee45058410387dda5ebb467425f5ced7790b1b9
InsaneDev2478 <vedansh.shetti@gmail.com>
2026-02-03 19:50:16 +0000

    added publish workflow

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,62 @@
+type CBType = () => {
+  res: boolean;
+  metadata?: any;
+  [key: string]: any;
+};
 
-type CBType=()=> {
-        res: boolean,
-        [key: string]: any
+function oTr(cb: CBType) {
+  const r = cb();
+  if (r.res) console.log("orTrue(...) passed.");
+  else console.error("orTrue(...) failed.");
+  return { orTrue: oTr, orFalse: oFls, end };
 }
 
-function oTr(cb: CBType){
-    const r=cb();
-    if(r.res) console.log("orTrue(...) passed.")
-    else console.error("orTrue(...) failed.")
-    return {orTrue: oTr, orFalse: oFls};
-}
-
-function oFls(cb: CBType){
-    const r=cb();
-    if(r.res) console.log("orTrue(...) passed.")
-    else console.error("orTrue(...) failed.")
-    return {orTrue: oTr, orFalse: oFls};
+function oFls(cb: CBType) {
+  const r = cb();
+  if (!r.res) console.log("orFalse(...) passed.");
+  else console.error("orFalse(...) failed.");
+  return { orTrue: oTr, orFalse: oFls, end };
 }
 
 function end() {
-    console.log("Test finished.");
+  console.log("Test finished.");
 }
 
 class TestSuite {
-    public whereTrue(cb: CBType) {
-        const r= cb();
-        if(r.res) console.log("whereTrue(...) passed!")
-        else console.error("whereTrue(...) failed.");
-        return {
-            prevMetadata: r.metadata,
-            orTrue: oTr,
-            orFalse: oFls,
-            end
-        }
-    }
-    public whereFalse(cb: CBType){
-        const r= cb();
-        if(!r.res) console.log("Test passed!")
-        else console.log("Test failed. ERR_WHEREFALSE_FAILED");
-        return {
-            prevReturn: r,
-            orTrue: oTr,
-            orFalse: oFls,
-            end
-        }
-    }
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  public whereTrue(cb: CBType) {
+    const r = cb();
+    if (r.res) console.log("whereTrue(...) passed!");
+    else console.error("whereTrue(...) failed.");
+
+    return {
+      prevMetadata: r.metadata,
+      orTrue: oTr,
+      orFalse: oFls,
+      end
+    };
+  }
+
+  public whereFalse(cb: CBType) {
+    const r = cb();
+    if (!r.res) console.log("whereFalse(...) passed!");
+    else console.error("whereFalse(...) failed. ERR_WHEREFALSE_FAILED");
+
+    return {
+      prevReturn: r,
+      orTrue: oTr,
+      orFalse: oFls,
+      end
+    };
+  }
 }
 
-export default function test(name: string){
-    console.log(`Starting test "${name}": `);
-    return new TestSuite();
+export default function test(name: string) {
+  console.log(`Starting test "${name}":`);
+  return new TestSuite(name);
 }
